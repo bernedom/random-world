948679a442532ddc8a44b82440a90dd845e8fe8b
Dominik Berner <dominik.berner@gmail.com>
2021-02-13 22:32:12 +0100

    Randomizing for one position

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     //let target_string: &'static str = "Random World!";
     let target_string: &'static str = "R";
     let mut output_string: String = "".to_string();
-    //let mut position_index = 0;
+    let mut position_index = 0;
     let mut iterations = 0;
 
     while output_string != target_string {
@@ -14,8 +14,18 @@ fn main() {
             .take(1)
             .map(char::from)
             .collect();
+        let current_char = target_string.chars().enumerate().nth(position_index);
+        if let Some(c) = &current_char {
+            if c.1.to_string() == rand_string {
+                output_string = rand_string;
 
-        output_string = rand_string;
+                position_index += 1
+            }
+        } else {
+            println!("Somethign went terribly wrong -> we already filled to correct string");
+            break;
+        }
+
         iterations += 1;
     }
 
