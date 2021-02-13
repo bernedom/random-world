38e6932cfe84cbb9b4fa88a042515dd6958d91f4
Dominik Berner <dominik.berner@gmail.com>
2021-02-13 21:57:19 +0100

    Working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let target_string: &'static str = "Random World!";
+    let mut output_string: String = "".to_string();
+    //let mut position_index = 0;
+
+    while output_string != target_string {
+        output_string = "Random World!".to_string();
+    }
+
+    println!("Found desired string '{}' ", output_string);
 }
