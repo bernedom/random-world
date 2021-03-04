e046a5ebe1889019fa92ac499ffff2e7a9992038
Dominik Berner <dominik.berner@bbv.ch>
2021-03-04 11:46:56 +0100

    Move closures out of loop

--- src/main.rs
+++ src/main.rs
@@ -9,28 +9,28 @@ fn main() {
     let mut iterations = 0;
     let mut rng = rand::thread_rng();
 
+    let mut retrieve_random = || -> char {
+        let idx = rng.gen_range(0..CHARSET.len());
+        CHARSET[idx] as char
+    };
+
+    let mut match_random = |current_char| -> (u32, char) {
+        let mut rand_string = retrieve_random();
+        let mut iterations = 1;
+        while current_char != rand_string {
+            rand_string = retrieve_random();
+            iterations+=1;
+        }
+        (iterations, rand_string)
+    };
+
     for current_char in target_string.chars() {
         assert!(
             CHARSET.contains(&(current_char as u8)),
             "Character to be found is in available charset"
         );
-
-        let mut retrieve_random = || -> char {
-            let idx = rng.gen_range(0..CHARSET.len());
-            CHARSET[idx] as char
-        };
-
-        let mut match_random = || -> (u32, char) {
-            let mut rand_string = retrieve_random();
-            let mut iterations = 1;
-            while current_char != rand_string {
-                rand_string = retrieve_random();
-                iterations+=1;
-            }
-            (iterations, rand_string)
-        };
                 
-        let match_result = match_random();
+        let match_result = match_random(current_char);
         
         output_string.insert(output_string.len(), match_result.1);
         iterations += match_result.0;
